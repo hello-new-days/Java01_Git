e6ac9d78d01fe66785ad6ee7c1ea6f59e8dde4c1
XiaoKun <2645425075@qq.com>
2022-10-30 00:33:15 +0800

    second commit

--- src/test/java/com/ck/commen/Test01.java
+++ src/test/java/com/ck/commen/Test01.java
@@ -3,5 +3,6 @@ package com.ck.commen;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello gihub");
     }
 }
