9a6c6fe944f8e4b5cb51e2a6eda3f92080f88aa0
XiaoKun <2645425075@qq.com>
2022-10-30 13:12:24 +0800

    Fourth commit

--- src/test/java/com/ck/commen/Test01.java
+++ src/test/java/com/ck/commen/Test01.java
@@ -6,5 +6,6 @@ public class Test01 {
         System.out.println("hello gihub");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
