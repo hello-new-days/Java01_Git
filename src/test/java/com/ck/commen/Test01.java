9d8431e094236aa8531d3f689822f40b3ba6ce63
hello-new-days <59903768+hello-new-days@users.noreply.github.com>
2022-10-30 10:30:15 +0800

    Update Test01.java

--- src/test/java/com/ck/commen/Test01.java
+++ src/test/java/com/ck/commen/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
         System.out.println("hello git");
         System.out.println("hello gihub");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
